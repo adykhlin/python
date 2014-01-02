0c2a51ab2e3715061c30267056d225fa15b288c3
Makhauser <dev@makhauser.com>
2014-01-02 21:39:28 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,7 +1,8 @@
 #!/usr/local/bin/python3
 # coding=utf-8
 
-import random
+import random,time
+
 def sticks_amount():
     '''Let's decide the amount of sticks for a game'''
     amount = ' '
@@ -17,7 +18,19 @@ def sticks_amount():
     else:
         print ("Impossible amount, we will play with the default amount of sticks (20)")
         return 20 
-amount=sticks_amount()
+
+def again():
+    '''We just ask a person for another game'''
+    global amount
+    print("Do you want to play again?")
+    if input().lower().startswith('y'):
+        amount=sticks_amount()
+        goes_first()
+    else:
+        print ("Thanks for playing")
+        print()
+        time.sleep(0.5)
+        quit()
 
 def game():
     '''Just what we need to grab sticks'''
@@ -33,13 +46,14 @@ def game():
     amount = amount - move
     if amount <= 0:
         print ("You have lost!")
-        quit()
+        #quit()
+        again()        
     print ("Left:"+amount*" |", amount)
     pc_move()
     return amount
-  
 
 def pc_move():
+    '''Game AI'''
     global amount
     if amount > 1 and amount < 5:
         amount = 1
@@ -49,14 +63,16 @@ def pc_move():
         amount -= 3
         if amount <= 0:
             print ("I have lost!")
-            quit()
+            #quit()
+            again()
         print("So, I move")
         print ("Left:"+amount*" |", amount)
     elif (amount-1)%4 == 0:
         amount -= random.randint(1,3)
         if amount <= 0:
             print ("I have lost!")
-            quit()
+            #quit()
+            again()
         print("So, I move")
         print ("Left:"+amount*" |", amount)
     else:
@@ -67,20 +83,22 @@ def pc_move():
         amount -= (small-1)
         if amount <= 0:
             print ("I have lost!")
-            quit()
+            #quit()
+            again()
         print("So, I move")
         print ("Left:"+amount*" |", amount)
     game()
     return amount
         
 def goes_first():
+    '''Random decision for the first move'''
     r = random.randint(0,1)
-    if r ==0:
+    if r == 0:
         print ("PC goes first")
         pc_move()
     else:
         print ("Player goes first")
         game()
+amount=sticks_amount()
 print ("Left:"+amount*" |", amount)
 goes_first()
-
